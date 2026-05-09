8b524bdc8b6d8f43b32ccb0cec4fc8cbb3340000
seungbum.cha <seungbum.cha@gmail.com>
2026-05-09 17:20:32 +0900

    Disable sitemap temporarily to fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sanjangboarder.github.io',
-  integrations: [sitemap()],
+  integrations: [
+    // sitemap()
+  ],
 });
